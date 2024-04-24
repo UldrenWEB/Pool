d53bc68e8d4e6f2711750503c254730116a910b0
UldrenWEB <uldrenmiguel33@gmail.com>
2024-04-24 19:53:24 -0400

    Primer commit

---
+++ Pool/Pool/DBConnection.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System;
+using System.Windows;
+
+namespace Pool
+{
+    class DBConnection
+    {
+        public NpgsqlConnection Conectar(String connectionString)
+        {
+
+            string cadenaConection = connectionString;
+
+            NpgsqlConnection conection = new NpgsqlConnection();
+            try
+            {
+                conection.ConnectionString = cadenaConection;
+                conection.Open();
+
+                //MessageBox.Show("Se ha conectado correctamente a la base de datos");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No se pudo establecer conexion con la base de datos: " + e.Message);
+            }
+
+
+            return conection;
+        }
+
+
+
+    }
+}
---
+++ Pool/Pool/Pool.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+
+namespace Pool
+{
+    class Pool
+    {
+        private static Pool? instance = null;
+
+        private Pool() { }
+
+        //Patron de diseño singleton
+
+        public static Pool GetInstance()
+        {
+            //Aqui usamos una asignacion compuesta de C# que significa que si instance es igual a null
+            //entonces sera igual a esa instancia
+            instance ??= new Pool();
+
+
+            return instance;
+        }
+
+
+        public List<NpgsqlConnection> CreatePool()
+        {
+
+            List<NpgsqlConnection> connects = new();
+
+
+            return connects;
+        }
+    }
+}
